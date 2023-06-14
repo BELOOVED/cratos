275e5e0d275acf5787a2e913a5b163773eb9286e
BELOOOVED <aycahanim@outlook.com>
2023-06-14 08:03:55 +0300

    modified:   m/index.php

--- m/index.php
+++ m/index.php
@@ -6,6 +6,6 @@ $page='main';
 $qpage=$q[0];
 if(!empty($qpage) && file_exists('view/'.$qpage.'.php')){$page=$qpage;}
 if(!empty($qpage) && file_exists('oyunlar/'.$qpage.'.php')){$page=$qpage;}
-include "modules/header.php";
+include "view/modules/header.php";
 include "view/".$page.".php";
-include "modules/footer.php";
+include "view/modules/footer.php";
